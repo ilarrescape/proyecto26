a94e34ac07022d1abc1177e99163be6bbcf4fd8a
Mauricio Ilarrescape <ilarrescapem@gmail.com>
2023-10-26 22:25:08 -0300

    commit 8

---
+++ proyectoEducacion/usuarios/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Pais(models.Model): 
+    idPais_id = models.IntegerField(primary_key = True) 
+    nombrePais = models.CharField(max_length=100, unique=True)
+
+class Provincia(models.Model): 
+    idProvincia_id = models.IntegerField(primary_key = True) 
+    nombreProvincia = models.CharField(max_length=100) 
+    fkPais = models.ForeignKey(Pais, on_delete=models.CASCADE)
+
+class Ciudad(models.Model): 
+    idCiudad_id = models.IntegerField(primary_key = True) 
+    nombreCiudad = models.CharField(max_length=100) 
+    fk_Provincia = models.ForeignKey(Provincia, on_delete=models.CASCADE) 
+    fk_Pais = models.ForeignKey(Pais, on_delete=models.CASCADE)
