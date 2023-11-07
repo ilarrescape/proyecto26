d123dee2d98ee19ceaf9318f1c6112f927e4f228
Mauricio Ilarrescape <ilarrescapem@gmail.com>
2023-11-07 11:08:09 -0300

    Crud previo clase 42 - parte II

--- proyectoEducacion/usuarios/static/usuarios/js/personas.js
+++ proyectoEducacion/usuarios/static/usuarios/js/personas.js
@@ -1,8 +1,18 @@
-$(function () {
-    $(".js-create-persona").click(function () {
-        $.ajax({ url: '/usuarios/agregar/', type: 'get', dataType: 'json', beforeSend: function () 
-        {$("#modal-persona").modal("show"); }, success: function (data) 
-            { $("#modal-persona .modal-content").html(data.html_form); } 
-        }); 
-    }); 
+$("#modal-persona").on("submit", ".js-create-persona", function () {
+    var form = $(this);
+    $.ajax({
+        url: form.attr("action"),
+        data: form.serialize(),
+        type: form.attr("method"),
+        dataType: 'json',
+        success: function (data) {
+            if (data.form_is_valid) {
+                alert("La persona fue creada");
+            }
+            else {
+                $("#modal-persona .modal-content").html(data.html_form);
+            }
+        }
+    });
+    return false;
 });
